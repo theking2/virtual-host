018a1b87dfaa3e504bb96314f2ed409337ed2fbf
jkingma <kingma@sbw-media.ch>
2020-12-01 11:18:56 +0100

    Projektdateien hinzufügen.

---
+++ VirtualHost/fmrMain.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace VirtualHost
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHostname = new System.Windows.Forms.Label();
+            this.txtHostname = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblHostname
+            // 
+            this.lblHostname.AutoSize = true;
+            this.lblHostname.Location = new System.Drawing.Point(30, 66);
+            this.lblHostname.Name = "lblHostname";
+            this.lblHostname.Size = new System.Drawing.Size(55, 13);
+            this.lblHostname.TabIndex = 0;
+            this.lblHostname.Text = "Hostname";
+            // 
+            // txtHostname
+            // 
+            this.txtHostname.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtHostname.Location = new System.Drawing.Point(33, 82);
+            this.txtHostname.Name = "txtHostname";
+            this.txtHostname.Size = new System.Drawing.Size(351, 20);
+            this.txtHostname.TabIndex = 1;
+            this.txtHostname.Validating += new System.ComponentModel.CancelEventHandler(this.txtHostname_Validating);
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(33, 120);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "Copy";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            this.btnOk.Enter += new System.EventHandler(this.btnOk_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(30, 13);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(29, 13);
+            this.lblPath.TabIndex = 3;
+            this.lblPath.Text = "Path";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Enabled = false;
+            this.txtPath.Location = new System.Drawing.Point(33, 29);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(351, 20);
+            this.txtPath.TabIndex = 4;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 174);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtHostname);
+            this.Controls.Add(this.lblHostname);
+            this.Name = "frmMain";
+            this.Text = "Add VirtualHost";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHostname;
+        private System.Windows.Forms.TextBox txtHostname;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.TextBox txtPath;
+    }
+}
+
---
+++ VirtualHost/fmrMain.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VirtualHost
+{
+    public partial class frmMain : Form
+    {
+        /** Path to root of host */
+        private string Path;
+        /** Name of host to add **/
+        private string Hostname;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            string[] args = Environment.GetCommandLineArgs();
+            if( args.Length < 2 )
+            {
+                MessageBox.Show("Need at least one argument.");
+                Application.Exit();
+            }
+
+            this.Path = args[1].Replace('\\', '/');
+            this.txtPath.Text = this.Path;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            this.Hostname = this.txtHostname.Text;
+
+            String result = String.Format( TEMPLATE, this.Path, this.Hostname );
+            Clipboard.SetDataObject(result);
+
+            MessageBox.Show(String.Format("vhost-Setup copied to Cliboard\r\nAdd the following line to the \\etc\\hosts file:\r\n127.0.0.1\t{0}", this.Hostname ) );
+            using (StreamWriter hosts = new StreamWriter("C:\\Windows\\System32\\drivers\\etc\\hosts", append:true))
+            {
+                hosts.WriteLine();
+                hosts.WriteLine("#Added by VirtualHost\r\n" + this.Hostname);
+                hosts.WriteLine("127.0.0.1\t" + this.Hostname);
+                hosts.WriteLine("::1\t" + this.Hostname);
+                hosts.WriteLine("#End added by VirtualHosts");
+                hosts.WriteLine();
+
+                hosts.Close();
+            }
+
+            Application.Exit();
+        }
+
+        const string TEMPLATE = @"#
+<VirtualHost *:80>
+    ServerName {1}
+
+    DocumentRoot ""{0}""
+    <Directory  ""{0}"">
+        Options +Indexes +Includes +FollowSymLinks +MultiViews
+
+        AllowOverride All
+
+        Require local
+    </Directory>
+</VirtualHost>
+";
+
+        private void txtHostname_Validating(object sender, CancelEventArgs e)
+        {
+            e.Cancel = txtHostname.Text.Length == 0;
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
